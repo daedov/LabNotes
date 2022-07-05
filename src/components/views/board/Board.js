9a777edfb13d26d99726a5945bb259bca31c39c9
daedov <daniqanil@gmail.com>
2022-07-05 12:53:08 -0400

    added lost import

--- src/components/views/board/Board.js
+++ src/components/views/board/Board.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Form, Container, Col, Row, Button, Card} from "react-bootstrap";
 
 function Board() {
@@ -8,23 +9,16 @@ function Board() {
         <Row>
           <Col>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Title Note</Form.Label>
+              <Form.Label><i className="material-icons">border_color</i> Title Note</Form.Label>
               <Form.Control type="text" />
-              <Form.TextForm className="text-muted">
-                Add a title for your note!
-              </Form.TextForm>
             </Form.Group>
           </Col>
           <Col>
             <Form.Group
               className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
+              controlId="exampleForm.ControlTextarea1">
               <Form.Label>Description Note</Form.Label>
               <Form.Control as="textarea" rows={3} />
-              <Form.TextForm className="text-muted">
-                Add a description for your Task!
-              </Form.TextForm>
             </Form.Group>
           </Col>
         </Row>
@@ -34,16 +28,15 @@ function Board() {
       <Card className="m-3">
         <Card.Img variant="top" src="https://picsum.photos/200/50" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Title Task</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Task Description...
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="secondary">Read more</Button>
         </Card.Body>
       </Card>
     </Container>
   );
-}
+};
 
 export default Board;
