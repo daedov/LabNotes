77314d98a64af0b8213bde492fdbb1d3f87b16f1
daedov <daniqanil@gmail.com>
2022-07-13 20:28:50 -0400

    style changes

--- src/layout/NotFound.jsx
+++ src/layout/NotFound.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import styles from './Layout.module.css';
 
 const NotFound = () => {
   return (
     <Container className={styles.container}>
-      <h3 className={styles.title}>404 Not Found!</h3>
+      <h3 className="font-bold text-4xl tracking-wide text-fuchsia-900">404 Not Found!</h3>
       <Link to="/">
-        <Button variant="secondary" className={styles.btn}>
+        <p className="font-mono  text-2xl text-center p-2 mx-1 text-sm text-purple-600 font-semibold hover:text-purple-400 hover:bg-purple-100 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
           Go back
-        </Button>
+        </p>
       </Link>
     </Container>
   );
