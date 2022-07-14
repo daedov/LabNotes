47b93ef4d716558129880c0c07938624a6cda619
daedov <daniqanil@gmail.com>
2022-07-13 20:30:08 -0400

    changes in the view of notes form

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { saveNote, getNote, updateNote } from '../firebase/noteProcess';
-import { useParams, useNavigate } from 'react-router-dom';
+// import AlertNote from '../layout/AlertNote'
 
 const NoteForm = () => {
   const initialValues = {
@@ -15,67 +15,86 @@ const NoteForm = () => {
   const handleChange = ({ target: { name, value } }) => {
     setNote({
       ...note,
-      [name]: value
+      [name]: value,
     });
-  }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(!params.id) {
+    if (!params.id) {
       await saveNote(note);
     } else {
       await updateNote(note, params.id);
     }
     setNote(initialValues);
     navigate('/ShowDashboard');
-  }
+  };
   const getNoteById = async (id) => {
     try {
-      const note = await getNote(id); 
-      setNote({ ...note.data()});
+      const note = await getNote(id);
+      setNote({ ...note.data() });
     } catch (error) {
       throw error.message;
     }
-  }
+  };
   useEffect(() => {
-    if(params.id) {
+    if (params.id) {
       getNoteById(params.id);
     }
   }, [params.id]);
 
-  return(
-    <Container className="board m-5 p-5">
-      <div>
-        <h3>Create a New Note</h3>
-        <Form onSubmit={handleSubmit}>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Title Note</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="title"
-                  value={note.title}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Description Note</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="description"
-                  value={note.description}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Button variant="secondary" type="submit">Add Note</Button>
-        </Form>
-      </div>
-    </Container>
-  )
-}
+  return (
+    <div className="w-full max-w-xs m-auto place-items-center bg-purple-50 rounded p-6 flex flex-col justify-center">
+      <form onSubmit={handleSubmit}>
+        <h3 className="font-bold text-2xl tracking-wide text-fuchsia-900 mb-4 text-center">
+          Your note is...
+        </h3>
+        <label className="block">
+          <span className="block font-bold text-sm text-fuchsia-900">Note Title </span>
+          <input
+            type="text"
+            name="title"
+            value={note.title}
+            onChange={handleChange}
+            className="mt-2 mb-3 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "
+          />
+        </label>
+        <label className="block">
+          <span className="block font-bold text-sm text-fuchsia-900">Note Description</span>
+          <input
+            type="text"
+            name="description"
+            value={note.description}
+            onChange={handleChange}
+            className="mt-2 mb-3 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "
+          />
+        </label>
 
-export default NoteForm; 
+        <button
+                        className="px-3 py-2 mx-1 mt-3 text-sm text-purple-900 bg-violet-200 font-semibold rounded-full border border-purple-300 hover:text-white hover:bg-violet-300 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          type="submit"
+        >
+          Add Note
+        </button>
+      </form>
+      <div>
+        <Link to="/ShowDashboard">
+          <p className="font-mono text-center p-2 mt-4 mx-1 text-sm text-purple-600 font-semibold hover:text-purple-400 hover:bg-purple-100 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Go back
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NoteForm;
